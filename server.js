6e8521b73a659bc7c7c202a852473f35c8b1646b
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-02-19 01:40:59 -0800

    some changes

--- server.js
+++ server.js
@@ -10,16 +10,13 @@ app.use(express.urlencoded({extended:true}));
 //2: Session code 
 
 //3: Views code
-app.set(":view","views");
+app.set("views","views");
 app.set('view engine',"ejs")
 
 //4 Routing code
-app.get("/hello", function(req, res) {
-res.end(`<h1 style="background: red">HELLO WORLD by BekzodAli</h1>`);
-});
-app.get("/gift", function(req, res) {
-    res.end(`<h1 style="background: red">Siz sovgalar bolimidasiz</h1>`);
-    });
+app.get("/", function (req,res){
+    res.render('harid');
+})
 
 const server = http.createServer(app);
 let PORT = 3000;
